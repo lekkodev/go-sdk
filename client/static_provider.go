2f4f13ae93af34902eb01d200f95da60c10c2fb4
Jonathan Dobbie <jonathan@lekko.com>
2023-12-14 17:08:11 -0800

    uhhh

--- client/static_provider.go
+++ client/static_provider.go
@@ -1,4 +1,4 @@
-// Copyright 2023 Lekko Technologies, Inc.
+// Copyright 2022 Lekko Technologies, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -24,7 +24,6 @@ import (
 	"google.golang.org/protobuf/types/known/structpb"
 	"google.golang.org/protobuf/types/known/wrapperspb"
 )
-
 
 func StaticProvider(featureBytes map[string]map[string][]byte) (Provider, error) {
 	backend, err := memory.NewStaticStore(featureBytes)
--- internal/memory/static.go
+++ internal/memory/static.go
@@ -1,4 +1,4 @@
-// Copyright 2023 Lekko Technologies, Inc.
+// Copyright 2022 Lekko Technologies, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,50 +16,48 @@ package memory
 
 import (
 	"context"
-    "errors"
+	"errors"
 
-    featurev1beta1 "buf.build/gen/go/lekkodev/cli/protocolbuffers/go/lekko/feature/v1beta1"
-    "github.com/lekkodev/go-sdk/pkg/eval"
+	featurev1beta1 "buf.build/gen/go/lekkodev/cli/protocolbuffers/go/lekko/feature/v1beta1"
+	"github.com/lekkodev/go-sdk/pkg/eval"
 	"google.golang.org/protobuf/proto"
 )
 
 func NewStaticStore(featureBytes map[string]map[string][]byte) (Store, error) {
-    features := make(map[string]map[string]*featurev1beta1.Feature)
-    for ns, fm := range featureBytes {
-        features[ns] = make(map[string]*featurev1beta1.Feature)
-        for key, fb := range fm {
-            f := &featurev1beta1.Feature{}
-            err := proto.Unmarshal(fb, f)
-            if err != nil {
-                return nil, err
-            }
-            features[ns][key] = f
-        }
-    }
-    return &staticStore{features}, nil
+	features := make(map[string]map[string]*featurev1beta1.Feature)
+	for ns, fm := range featureBytes {
+		features[ns] = make(map[string]*featurev1beta1.Feature)
+		for key, fb := range fm {
+			f := &featurev1beta1.Feature{}
+			err := proto.Unmarshal(fb, f)
+			if err != nil {
+				return nil, err
+			}
+			features[ns][key] = f
+		}
+	}
+	return &staticStore{features}, nil
 }
-
 
 type staticStore struct {
-    features map[string]map[string]*featurev1beta1.Feature
+	features map[string]map[string]*featurev1beta1.Feature
 }
 
-
 func (s *staticStore) Evaluate(key string, namespace string, lekkoContext map[string]interface{}, dest proto.Message) error {
-    ns, ok := s.features[namespace]
-    if !ok {
-        return errors.New("Unknown Namespace")
-    }
-    cfg, ok := ns[key]
-    if !ok {
-        return errors.New("Unknown Key")
-    }
-    evaluableConfig := eval.NewV1Beta3(cfg, namespace)
-    a, _, err := evaluableConfig.Evaluate(lekkoContext)
-    if err != nil {
-        return err
-    }
-    return a.UnmarshalTo(dest)
+	ns, ok := s.features[namespace]
+	if !ok {
+		return errors.New("Unknown Namespace")
+	}
+	cfg, ok := ns[key]
+	if !ok {
+		return errors.New("Unknown Key")
+	}
+	evaluableConfig := eval.NewV1Beta3(cfg, namespace)
+	a, _, err := evaluableConfig.Evaluate(lekkoContext)
+	if err != nil {
+		return err
+	}
+	return a.UnmarshalTo(dest)
 }
 
 func (s *staticStore) Close(ctx context.Context) error {
